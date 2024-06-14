d71eedd6f3212bfb1b2913f1dbc911e6ef7fea01
govindchari <govindchari1@gmail.com>
2024-06-13 19:23:13 -0700

    Only use ints and doubles

--- include/definitions.h
+++ include/definitions.h
@@ -17,22 +17,11 @@
 
 // Define QCOSInt and QCOSFloat.
 #include <limits.h>
-#ifdef QCOS_LONG_INT
-typedef long long QCOSInt;
-#define QCOSInt_MAX LLONG_MAX
-#else
 typedef int QCOSInt;
 #define QCOSInt_MAX INT_MAX
-#endif
 
-#ifdef QCOS_SINGLE_PRECISION
-typedef float QCOSFloat;
-#define QCOSFloat_MAX __FLT_MAX__
-#else
 typedef double QCOSFloat;
 #define QCOSFloat_MAX __DBL_MAX__
-
-#endif
 
 #define qcos_max(a, b) (((a) > (b)) ? (a) : (b))
 #define qcos_min(a, b) (((a) < (b)) ? (a) : (b))
